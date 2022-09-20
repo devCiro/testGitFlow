72ff40b99b40c948b01984cc3f20080418abfde4
CiroDev1987 <cirodev.junior@gmail.com>
2022-09-20 09:40:09 -0300

    first commit

---
+++ src/main/java/com/teste_git_flow/TesteGitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.teste_git_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteGitFlowApplication.class, args);
+    }
+
+}
